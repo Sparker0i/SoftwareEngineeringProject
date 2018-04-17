55d3d47b1a04938d35ef6e87fcb5fe1026b29817
Aaditya Menon <aaditya@sparker0i.me>
2018-04-17 22:09:24 +0530

    Corrected Syntax viz a viz local_settings.json

--- sql.py
+++ sql.py
@@ -8,7 +8,7 @@ class DumpToSQL:
         with open('local_settings.json') as jsonfile:
             data = json.load(jsonfile)
             host = data['host']
-            username = data['username']
+            username = data['user']
             password = data['password']
             database = data['database']
         self.db = my.connect(host , username , password , database , charset="utf8mb4", use_unicode=True)
