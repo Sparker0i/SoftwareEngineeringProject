cb36c3218cfe4f830d28eeb73bdf0d66e13d1902
Aaditya Menon <aaditya@sparker0i.me>
2018-04-21 20:34:05 +0530

    Added Comments for Classes and their respective functions

--- classes.py
+++ classes.py
@@ -9,12 +9,26 @@ class XMLParser:
         return tree
 
 class DataExtractor:
+
+    # Finding the elements should always happen along with the XMLNS. Syntax: object.find('{http://www.drugbank.ca}colname')
     
+    # Checks Every Drug. If every required value is present, it returns true, along with a new object of the Drug class
     def check_drug(self , drug):
         if drug.find('{http://www.drugbank.ca}name').text and drug.find('{http://www.drugbank.ca}drugbank-id').text:
             return (True , Drug(drug.find('{http://www.drugbank.ca}name').text , drug.find('{http://www.drugbank.ca}drugbank-id').text))
         return False
+
+    #To find what elements to search for, refer to models.py and also their respective names in the XML file 
+    def check_drugclass(self , drug):
+        print()
     
+    def check_drugtarget(self , drug):
+        print()
+
+    def check_interactions(self , drug):
+        print()
+    
+    #TODO: Modify this function to check for all - Drug , DrugClass , DrugTarget, Interactions
     def initialize_classes(self , tree):
         root = tree.getroot()
         for drug in root.getchildren():
