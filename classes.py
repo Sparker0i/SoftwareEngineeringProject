1c1d824f77e6937b4fc1d88da448b93e9b9b6bc9
Aaditya Menon <aaditya@sparker0i.me>
2018-04-25 00:49:11 +0530

    Added Target Loop

--- classes.py
+++ classes.py
@@ -21,22 +21,29 @@ class DataExtractor:
     #To find what elements to search for, refer to models.py and also their respective names in the XML file
     def check_drugclass(self , drug):
         if drug.find('{http://www.drugbank.ca}direct-print').text and drug.find('{http://www.drugbank.ca}kingdom').text and drug.find('{http://www.drugbank.ca}superclass').text and drug.find('{http://www.drugbank.ca}class').text and drug.find('{http://www.drugbank.ca}drugbank-id').text:
-            return (True , Drug(drug.find('{http://www.drugbank.ca}direct-print').text , drug.find('{http://www.drugbank.ca}kingdom').text , drug.find('{http://www.drugbank.ca}superclass').text , drug.find('{http://www.drugbank.ca}class').text , drug.find('{http://www.drugbank.ca}drugbank-id').text))
+            return (True , DrugClass(drug.find('{http://www.drugbank.ca}direct-print').text , drug.find('{http://www.drugbank.ca}kingdom').text , drug.find('{http://www.drugbank.ca}superclass').text , drug.find('{http://www.drugbank.ca}class').text , drug.find('{http://www.drugbank.ca}drugbank-id').text))
         return False
 
     def check_drugtarget(self , drug):
-        if drug.find('{http://www.drugbank.ca}position').text and drug.find('{http://www.drugbank.ca}name').text and drug.find('{http://www.drugbank.ca}drugbank-id').text and drug.find('{http://www.drugbank.ca}id').text:
-            return (True, Drug(drug.find('{http://www.drugbank.ca}position').text , drug.find('{http://www.drugbank.ca}name').text , drug.find('{http://www.drugbank.ca}drugbank-id').text , drug.find('{http://www.drugbank.ca}id').text))
+        targets = drug.find('{http://www.drugbank.ca}targets')
+        for target in targets:
+            if drug.find('{http://www.drugbank.ca}position').text and drug.find('{http://www.drugbank.ca}name').text and drug.find('{http://www.drugbank.ca}drugbank-id').text and drug.find('{http://www.drugbank.ca}id').text:
+                return (True, DrugTarget(drug.find('{http://www.drugbank.ca}position').text , drug.find('{http://www.drugbank.ca}name').text , drug.find('{http://www.drugbank.ca}drugbank-id').text , drug.find('{http://www.drugbank.ca}id').text))
         return False
+    
     def check_interactions(self , drug):
         if drug.find('{http://www.drugbank.ca}id').text and drug.find('{http://www.drugbank.ca}name').text and drug.find('{http://www.drugbank.ca}drugbank-id').text and drug.find('{http://www.drugbank.ca}description').text:
-            return (True, Drug(drug.find('{http://www.drugbank.ca}id').text , drug.find('{http://www.drugbank.ca}name').text , drug.find('{http://www.drugbank.ca}drugbank-id').text , drug.find('{http://www.drugbank.ca}description').text))
+            return (True, DrugInteractions(drug.find('{http://www.drugbank.ca}id').text , drug.find('{http://www.drugbank.ca}name').text , drug.find('{http://www.drugbank.ca}drugbank-id').text , drug.find('{http://www.drugbank.ca}description').text))
         return False
+    
     #TODO: Modify this function to check for all - Drug , DrugClass , DrugTarget, Interactions
     def initialize_classes(self , tree):
         root = tree.getroot()
         for drug in root.getchildren():
             check , value = self.check_drug(drug)
+            check1 , value1 = self.check_drugclass(drug)
             if check:
                 sql.DumpToSQL().insert_drug(value)
+            if check1:
+                sql.DumpToSQL().insert_drug(value1)
 
